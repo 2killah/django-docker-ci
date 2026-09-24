cb97b350de8ba1a049ef0bacc15ca0cfa2e16cda
2killah <adelya.tn@gmail.com>
2026-09-24 10:16:05 +0500

    hshs

---
+++ api/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+class HealthTest(TestCase):
+
+    def test_health_api(self):
+        response = self.client.get("/api/health/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["status"], "ok")
